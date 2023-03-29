bfad074cb1d78c7cb81a82dc63537d62b62ba923
mateus52 <54671061+mateus52@users.noreply.github.com>
2023-03-29 19:24:45 -0300

    Initial commit

---
+++ src/main/java/Hamming.java
@@ -0,0 +1,26 @@
+public class Hamming {
+    private  String leftStrand;
+    private  String rightStrand;
+    
+    public Hamming(String leftStrand, String rightStrand) {
+      if (!leftStrand.isEmpty() && rightStrand.isEmpty()) throw new IllegalArgumentException("right strand must not be empty.");
+        if (!rightStrand.isEmpty() && leftStrand.isEmpty()) throw new IllegalArgumentException("left strand must not be empty.");
+        if (leftStrand.length() != rightStrand.length()) throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+    this.leftStrand = leftStrand;
+    this.rightStrand = rightStrand;
+}
+
+    public int getHammingDistance() {
+        
+        //long distancia = IntStream.range(0, leftStrand.length()).filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i)).count();  
+
+        int distancia = 0;
+        
+        for (int i = 0; i < rightStrand.length(); i++){
+            if (rightStrand.charAt(i) != leftStrand.charAt(i)){
+                distancia++;
+            }
+        }
+        return distancia;
+    }
+}
